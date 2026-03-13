cedeba692318e23cd5cec731b36b74a84ef7ce98
Dineesh <kesavaraapudineesh@gmail.com>
2026-03-13 14:37:56 +0530

    global was added

---
+++ terraform-templet/global/variable.tf
@@ -0,0 +1,14 @@
+variable "name" {
+    type = string
+    default = "Pickle-Project"
+}
+variable "environment" {
+    type = string
+    default = "dev"
+  
+}
+variable "owner" {
+    type = string
+    default = "dineesh"
+  
+}
